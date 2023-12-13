e3cbe1fb44d8d72141485d53eab3d8a2f0186867
James Harbin <james.harbin@york.ac.uk>
2023-12-13 11:36:40 +0000

    Changes for Docker Desktop

---
+++ eddi-docker/desktop/kill-eddi-desktop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker compose down
+sleep 10
+# Only terminate the ConSerts - leave the Dockers for Kafka running!
+docker ps -f "name=consert" -aq | xargs docker stop | xargs docker rm | echo
+echo "Done"
